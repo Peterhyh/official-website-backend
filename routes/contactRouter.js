2724962550b8c153110ca1c5339103365a3c765a
Peter Huynh <peterhyh@yahoo.com>
2023-03-10 20:26:29 -0600

    contact router added

---
+++ routes/contactRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const Contact = require('../model/contact');
+
+const contactRouter = express.Router();
+
+contactRouter.route('/')
+    .get((req, res) => {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('GET method not supported on /contact');
+    })
+
+    .post((req, res, next) => {
+        Contact.create(req.body)
+            .then(contact => {
+                console.log("-------LOG:", contact);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(`${contact} was sent to Peter!`)
+            })
+            .catch(err => next(err))
+    })
+
+    .put((req, res) => {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('PUT method not supported on /contact');
+    })
+
+    .delete((req, res) => {
+        res.statusCode = 403;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('DELETE method not supported on /contact');
+    })
+
+module.exports = contactRouter;
